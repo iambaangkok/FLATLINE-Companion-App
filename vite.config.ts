e1553e815ea32f48e5d7ea0e9becc070a5d63990
Baangkok Vanijyananda <40522040+iambaangkok@users.noreply.github.com>
2024-08-29 14:13:26 +0700

    add: vite-plugin-gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ghPages } from "vite-plugin-gh-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "/FLATLINE-Companion-App/",
+  plugins: [react(), ghPages({
+    branch: "master",
+    dest: "/docs",
+  })],
 })
